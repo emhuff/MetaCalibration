be0c29a2f0f31cbd3d832b6dfd7b2d0f4d2f75df
emhuff <emhuff@gmail.com>
2015-11-20 13:22:31 -0500

    aaargh

--- Scripts/shear_est_simple_sheldon.py
+++ Scripts/shear_est_simple_sheldon.py
@@ -98,7 +98,7 @@ def ml_g_est( e=None, g_start = 0.,mu_coeff=None,sigma_coeff = None, nu_coeff = 
         return -np.mean(np.log(prob[use])) 
     # find the value of g, given the coefficients, that maximizes <-logL>
     result = minimize_scalar(logL_t,method='Bounded',bounds=[-0.1,0.1])
-
+    stop
     return np.asscalar(result.x)
 
 def shear_est(catalogs, truthTable, delta_g = 0.01, weights = True,mc_type=None):
